ea37d1c7cf446464e9ad21f62ad69ca2244d7c8c
Mohammad Arsalan <arsalancode@outlook.com>
2018-02-15 01:08:36 +0530

    Initial Check in

---
+++ ProblemSolving/Graphs/CycleDetectionDirectedGraph.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProblemSolving.Graphs
+{
+    partial class DirectedGraph
+    {
+        Dictionary<int,List<int>> graph;
+        int count;
+        public DirectedGraph(int count)
+        {
+            this.count = count;
+            graph = new Dictionary<int,List<int>>();
+        }
+
+        public void AddEdge(int x, int y)
+        {
+            if (!graph.ContainsKey(x))
+            {
+                graph.Add(x, new List<int>());
+            }
+
+            graph[x].Add(y);
+        }
+
+        public void DetectCycle()
+        {
+
+            bool[] visited = new bool[count];
+            bool[] recStack = new bool[count];
+            for (int i = 0; i < count; i++)
+            {
+                visited[i] = false;
+                recStack[i] = false;
+            }
+
+            bool cyclic = false;
+            for (int i = 0; i < count; i++)
+            {
+               cyclic = IsCyclicUtil(i, visited, recStack);
+            }
+
+            Console.WriteLine("Graph is cyclic -->>" + cyclic);
+            Console.ReadLine();
+        }
+
+        private bool IsCyclicUtil(int key, bool[] visited, bool[] recStack)
+        {
+            if (!visited[key])
+            {
+                visited[key] = true;
+                recStack[key] = true;
+                if (graph.ContainsKey(key))
+                {
+                    foreach (int key1 in graph[key])
+                    {
+                        if (IsCyclicUtil(key1, visited, recStack))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            else if (visited[key] && recStack[key])
+            {
+                recStack[key] = false;
+                return true;
+            }
+
+            recStack[key] = false;
+            return false;
+        }
+    }
+}
--- ProblemSolving/Program.cs
+++ ProblemSolving/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProblemSolving.Graphs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,14 @@ namespace ProblemSolving
     {
         static void Main(string[] args)
         {
+            DirectedGraph directedGraph = new DirectedGraph(4);
+            directedGraph.AddEdge(0, 1);
+            directedGraph.AddEdge(0, 2);
+            directedGraph.AddEdge(1, 2);
+            directedGraph.AddEdge(2, 0);
+            directedGraph.AddEdge(2, 3);
+            directedGraph.AddEdge(3, 3);
+            directedGraph.DetectCycle();
         }
     }
 }
