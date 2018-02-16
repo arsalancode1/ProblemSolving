7b5292f0b4f63d0e982b46f9398761d9e273785b
Mohammad Arsalan <arsalancode@outlook.com>
2018-02-16 20:49:14 +0530

    added solution for longest path in DAG.

---
+++ ProblemSolving/Graphs/LongestPathDAG.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving.Graphs
+{
+    partial class WeightedDirectedGraph
+    {
+        Dictionary<int, List<AdjListNode>> graph;
+        int count;
+
+        public WeightedDirectedGraph(int count)
+        {
+            graph = new Dictionary<int, List<AdjListNode>>();
+            this.count = count;
+        }
+
+        public void AddEdge(int u, int v, int weight)
+        {
+            if(!graph.ContainsKey(u))
+            {
+                graph.Add(u, new List<AdjListNode>());
+            }
+
+            graph[u].Add(new AdjListNode() { V = v, Weight = weight });
+        }
+
+        public void LongestPath(int source)
+        {
+            int[] longestPath = new int[count];
+            for (int i = 0; i < count; i++)
+            {
+                longestPath[i] = Int32.MinValue;
+            }
+
+            longestPath[source] = 0;
+
+            bool[] visited = new bool[count];
+            for (int i = 0; i < count; i++)
+            {
+                visited[i] = false;
+            }
+
+            Stack<int> topologicalSort = new Stack<int>();
+            for (int i = 0; i < count; i++)
+            {
+                SortUtil(i, visited, topologicalSort);
+            }
+
+            foreach(int u in topologicalSort)
+            {
+                if (graph.ContainsKey(u))
+                {
+                    foreach (AdjListNode v in graph[u])
+                    {
+                        if (longestPath[u] != Int32.MinValue)
+                        {
+                            if (longestPath[u] + v.Weight > longestPath[v.V])
+                            {
+                                longestPath[v.V] = longestPath[u] + v.Weight;
+                            }
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine("Longest path from source vertex {0} to destination vertex {1} is {2}", source, i, longestPath[i] != Int32.MinValue ? longestPath[i].ToString() : "NA");
+            }
+
+            Console.ReadLine();
+        }
+
+        public void SortUtil(int node, bool[] visited, Stack<int> stack)
+        {
+            if (!visited[node])
+            {
+                visited[node] = true;
+                if (graph.ContainsKey(node))
+                {
+                    foreach (AdjListNode key in graph[node])
+                    {
+                        SortUtil(key.V, visited, stack);
+                    }
+                }
+
+                stack.Push(node);
+            }
+        }
+    }
+
+    class AdjListNode
+    {
+        public int V { get; set; }
+        public int Weight { get; set; }
+    }
+}
--- ProblemSolving/Program.cs
+++ ProblemSolving/Program.cs
@@ -11,14 +11,18 @@ namespace ProblemSolving
     {
         static void Main(string[] args)
         {
-            DirectedGraph directedGraph = new DirectedGraph(4);
-            directedGraph.AddEdge(0, 1);
-            directedGraph.AddEdge(0, 2);
-            directedGraph.AddEdge(1, 2);
-            directedGraph.AddEdge(2, 0);
-            directedGraph.AddEdge(2, 3);
-            directedGraph.AddEdge(3, 3);
-            directedGraph.DetectCycle();
+            WeightedDirectedGraph g = new WeightedDirectedGraph(6);
+            g.AddEdge(0, 1, 5);
+            g.AddEdge(0, 2, 3);
+            g.AddEdge(1, 3, 6);
+            g.AddEdge(1, 2, 2);
+            g.AddEdge(2, 4, 4);
+            g.AddEdge(2, 5, 2);
+            g.AddEdge(2, 3, 7);
+            g.AddEdge(3, 5, 1);
+            g.AddEdge(3, 4, -1);
+            g.AddEdge(4, 5, -2);
+            g.LongestPath(1);
         }
     }
 }
