2fe190796cbbc192f266a43a2a6105e87e4c4967
Mohammad Arsalan <arsalancode@outlook.com>
2018-02-19 00:31:07 +0530

    added solution to calculate height of tree.

--- ProblemSolving/Program.cs
+++ ProblemSolving/Program.cs
@@ -1,4 +1,4 @@
-﻿using ProblemSolving.Graphs;
+﻿using ProblemSolving.Trees;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,18 +11,9 @@ namespace ProblemSolving
     {
         static void Main(string[] args)
         {
-            WeightedDirectedGraph g = new WeightedDirectedGraph(6);
-            g.AddEdge(0, 1, 5);
-            g.AddEdge(0, 2, 3);
-            g.AddEdge(1, 3, 6);
-            g.AddEdge(1, 2, 2);
-            g.AddEdge(2, 4, 4);
-            g.AddEdge(2, 5, 2);
-            g.AddEdge(2, 3, 7);
-            g.AddEdge(3, 5, 1);
-            g.AddEdge(3, 4, -1);
-            g.AddEdge(4, 5, -2);
-            g.LongestPath(1);
+
+            BinaryTree tree = new BinaryTree();
+            tree.CalculateHeight();
         }
     }
 }
---
+++ ProblemSolving/Trees/HeightOfTree.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving.Trees
+{
+    partial class BinaryTree
+    {
+        private int CalculateHeight(Node node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+
+            return 1 + Math.Max(CalculateHeight(node.LeftNode), CalculateHeight(node.RightNode));
+        }
+
+        public void CalculateHeight()
+        {
+            Console.WriteLine(CalculateHeight(root));
+            Console.ReadLine();
+        }
+    }
+}
